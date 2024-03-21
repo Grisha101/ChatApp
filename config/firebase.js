29e21ade3a5fe7a99aed610b9d626613cbb6fbae
Luca <36565154+Grisha101@users.noreply.github.com>
2024-03-21 03:19:58 +0200

    init

--- config/firebase.js
+++ config/firebase.js
@@ -4,7 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Firebase config
 const firebaseConfig = {
-
+  apiKey: "AIzaSyC5w2Rb91S8FLyrvH0rOiJG-VMe0HSNbQM",
+  authDomain: "msg-01-105f8.firebaseapp.com",
+  projectId: "msg-01-105f8",
+  storageBucket: "msg-01-105f8.appspot.com",
+  messagingSenderId: "72423144855",
+  appId: "1:72423144855:web:c82a591e1eac1889bd18d2"
 };
 // initialize firebase
 initializeApp(firebaseConfig);
